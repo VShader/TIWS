d1f32450d80fa231ae54190753e6865e38126e96
VShader <t.weitz66@web.de>
2013-11-17 12:47:26 +0100

    add exercise 34 for Uebung_7

---
+++ TIWS_Uebung/Uebung_7.pl
@@ -0,0 +1,12 @@
+% Autor:
+% Datum: 17.11.2013
+
+app([X|Xs],Ys,[X|Rs]) :- app(Xs,Ys,Rs).
+app([],Xs,Xs).
+
+%Aufgabe: 34
+members(X,[X|Ys]).
+members(X,[Y|Ys]) :- members(X,Ys).
+
+add_el(X,Ls,Rs) :- members(X,Ls), !, app([],Ls,Rs).
+add_el(X,Ls,Rs) :- app([X],Ls,Rs).
